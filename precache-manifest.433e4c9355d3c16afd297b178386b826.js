4f094136d4edc7ccfb053b03c62dadc2790903ee
rahatalom <68152354+rahatalom@users.noreply.github.com>
2020-09-16 22:20:56 +0100

    Updates

--- precache-manifest.433e4c9355d3c16afd297b178386b826.js
+++ precache-manifest.433e4c9355d3c16afd297b178386b826.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "386f011ae04862d984bbb275b547f1f2",
+    "revision": "7008583a4d7270764ea88467a784c504",
     "url": "/portfolio/index.html"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/js/2.06102427.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e7a9d9959abd302f29cf",
-    "url": "/portfolio/static/js/main.3948497a.chunk.js"
+    "revision": "bbd82810663c76a94271",
+    "url": "/portfolio/static/js/main.a529e98d.chunk.js"
   },
   {
     "revision": "7a6cc99bd8f08f9fa6ec",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.e36022224e9ec30ea5ebe2672cdc84e1.js"
+  "/portfolio/precache-manifest.433e4c9355d3c16afd297b178386b826.js"
 );
 
 self.addEventListener('message', (event) => {
