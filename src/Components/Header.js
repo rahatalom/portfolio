e6a4258fa7e50eaf3660f0d6df13052962cd0850
rahatalom <68152354+rahatalom@users.noreply.github.com>
2020-09-16 22:15:26 +0100

    css changes

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,7 +4,11 @@ function Header() {
   return (
     <div className="outerContainer">
       <div className="title">
-        <h1>Rahatul Alom</h1>
+        <div className="name">
+          {" "}
+          <h1>Rahatul Alom</h1>
+        </div>
+
         <h3>Front end engineer</h3>
         <button onClick="#proj" class="btn btn-danger btn-lg">
           <a className="see-proj" href="#proj">
