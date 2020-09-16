d6989509ea8127653586f60fe9c99f14e50d6a47
rahatalom <68152354+rahatalom@users.noreply.github.com>
2020-09-16 17:52:48 +0100

    Updates

---
+++ precache-manifest.e36022224e9ec30ea5ebe2672cdc84e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "386f011ae04862d984bbb275b547f1f2",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "26904555a508fd773d3e",
+    "url": "/portfolio/static/js/2.06102427.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portfolio/static/js/2.06102427.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7a9d9959abd302f29cf",
+    "url": "/portfolio/static/js/main.3948497a.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
